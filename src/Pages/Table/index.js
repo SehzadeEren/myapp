4c82f37540a5b832749430372a3ad29b9f38104f
SehzadeEren <65957685+SehzadeEren@users.noreply.github.com>
2022-08-17 23:52:46 +0300

    Header'a Routes eklendi

---
+++ src/Pages/Table/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+
+
+function Table() {
+
+ return (
+
+<div>
+   <h1>Table</h1>
+   
+    <Link to="/Table"> Table </Link> |{" "}
+    <Link to="/"> Contact </Link> 
+ 
+</div>
+
+
+    );
+}
+
+export default Table 
