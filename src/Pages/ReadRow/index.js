2e31e3ab47a7f99dce438bcae8802956e4a4714a
SehzadeEren <65957685+SehzadeEren@users.noreply.github.com>
2022-08-23 16:23:04 +0300

    delete butonu elendi

--- src/Pages/ReadRow/index.js
+++ src/Pages/ReadRow/index.js
@@ -1,16 +1,35 @@
 import React from "react";
+import { useContext } from "react";
+import { GlobalContext } from "../../Components/Context/context";
+import "../../../src/App.css";
+
+
 
 const ReadRow = ({ addForm }) => {
+    const { addFormData, setAddFormData } = useContext(GlobalContext);
+
+    const handleDeleteClick = (addFormId) => {
+        const newAddFormData = addFormData.filter((_, index) =>
+            index !== addFormId
+        )
+
+        setAddFormData(newAddFormData);
+    };
+
+
     return (
 
-        <div>
-            <tr key={i} >
-                <td>{addForm.fullName}</td>
-                <td>{addForm.email}</td>
-                <td>{addForm.phoneNumber}</td>
-                <td>{addForm.country}</td>
-            </tr>
-        </div>
+
+        <tr>
+            <td>{addForm.fullName}</td>
+            <td>{addForm.email}</td>
+            <td>{addForm.phoneNumber}</td>
+            <td>{addForm.country}</td>
+            <td>
+                <div className="icon-delete" onClick={() => handleDeleteClick(addForm.id)}> </div>
+            </td>
+        </tr>
+
     )
 }
 
